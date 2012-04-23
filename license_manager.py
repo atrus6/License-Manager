f44591a282e740edcf6f24bd1dede09ef8c1b528
Tim Butram <atrus6@gmail.com>
2012-04-23 00:37:20 -0400

    Added DB items

--- license_manager.py
+++ license_manager.py
@@ -13,6 +13,41 @@ def calculate_MD5(file_handler):
 
     return file_hash.digest()
 
+def create_table(cursor):
+    cursor.execute('CREATE TABLE media   \
+                (name TEXT,              \
+                md5 TEXT,                \
+                license TEXT,            \
+                file BLOB,               \
+                ancestor_name TEXT,      \
+                ancestor_md5 TEXT,       \
+                PRIMARY KEY(name, md5))')
+
+
+
+def insert_item(file_handle, filename, file_md5, license, ancestor_name, ancestor_md5, db):
+    con = None
+    cursor = None
+
+    if args.database == NoneType:
+        con = sqlite.connect('media.db')
+        cursor = con.cursor()
+
+        #Test if the table exists or we just opened up something new.
+        cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="media"')
+        table_exists = cursor.fetchone()
+
+        if table_exists == None:
+            create_table(cursor)
+    else:
+        con = sqlite.connect(db)
+        cursor = con.cursor()
+
+    cursor.execute('INSERT INTO media (name, md5, license, file, ancestorName, ancestorMD5)\
+            VALUES (?, ?, ?, ?, ?, ?)',
+            (filename, file_md5, license, ancestor_name, ancestor_md5))
+
+
 def main():
     parser = argparse.ArgumentParser(
             description='Handles media licensing information')
@@ -30,8 +65,14 @@ def main():
     parser_add.add_argument('-u', '--url', action='store', dest='url',
             help='The attribution URL for the media.')
     parser_add.add_argument('-a', '--ancestor', action='store', dest='ancestor',
-            nargs='?', type=argparse.FileType('r'),
+            type=argparse.FileType('r'),
             help='The file path of the parent of this derivitive work.')
+
+    parser_add.add_argument('-d', '--database', action='store', dest='database',
+            type=argparse.FileType('r+'),
+            help=('Database to work on. If none give, the assumed location is \
+                ./media.db'))
+
 
     args = parser.parse_args()
     #Calculate MD5 of the new file.
@@ -42,7 +83,7 @@ def main():
     ancestor_name = ancestor.name
     ancestor_md5 = calculate_MD5(ancestor)
 
-    #insert_item(args.filename, name, md5, license, ancestor_name, ancestor_md5)
+    insert_item(args.file, name, md5, license, ancestor_name, ancestor_md5, args.database)
 
 if __name__ == '__main__':
     main()
